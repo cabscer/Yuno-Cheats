74ef5e6095c7e29808e46d5c4edd9316d2c26bb8
Bloodmoon-gui <morriscaleb2@icloud.com>
2026-07-03 05:15:55 -0500

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,6 +1,13 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/cabscer/Yuno-Cheats/main/loader.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/cabscer/Yuno-Cheats/main/loader.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/cabscer/Yuno-Cheats/main/loader.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/cabscer/Yuno-Cheats/main/loader.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/cabscer/Yuno-Cheats/main/loader.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/cabscer/Yuno-Cheats/main/loader.lua"))()
+-- Main Lib
+loadstring(game:HttpGet("https://raw.githubusercontent.com/cabscer/Yuno-Cheats/refs/heads/main/Server.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/cabscer/Yuno-Cheats/refs/heads/main/Library_Server.lua"))()
+
+-- Mods
+loadstring(game:HttpGet("https://raw.githubusercontent.com/cabscer/Yuno-Cheats/refs/heads/main/mods/Combat_Server.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/cabscer/Yuno-Cheats/refs/heads/main/mods/Utility_Server.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/cabscer/Yuno-Cheats/refs/heads/main/mods/Visuals.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/cabscer/Yuno-Cheats/refs/heads/main/mods/Movement_Server.lua"))()
+
+-- Addons
+loadstring(game:HttpGet("https://raw.githubusercontent.com/cabscer/Yuno-Cheats/refs/heads/main/addons/SaveManager.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/cabscer/Yuno-Cheats/refs/heads/main/addons/ThemeManager.lua"))()
